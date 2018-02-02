a523abf5912ca0ea358a71781506a0f8e005fb5f
WslyStvnChng <mr.cheng.st@gmail.com^C>
2018-02-01 22:35:27 -0700

    first take on getting twitter tweets to post

---
+++ keys.js
@@ -0,0 +1,16 @@
+exports.twitter = {
+  consumer_key: "K5C5tSZyi2UTMIgSrxHbxOYeh",
+  consumer_secret: "CO2Fb75lbjuutJrbyNHE6gYIxgY8K5bcsAnRkMjM2HmSD24QC5",
+  access_token_key: "958520639815495681-NA5SwL3n9KM6Oz16ckPNHl4KfBxpF2g",
+  access_token_secret: "L7BuXaHMo3pZH9TXeomRJ9IlwjGUvUCDLLAq0noQJmmxV"
+};
+
+console.log("This works");
+// exports.twitter = {
+//   consumer_key: "process.env.K5C5tSZyi2UTMIgSrxHbxOYeh",
+//   consumer_secret: "process.env.CO2Fb75lbjuutJrbyNHE6gYIxgY8K5bcsAnRkMjM2HmSD24QC5",
+//   access_token_key: "process.env.958520639815495681-NA5SwL3n9KM6Oz16ckPNHl4KfBxpF2g",
+//   access_token_secret: "process.env.L7BuXaHMo3pZH9TXeomRJ9IlwjGUvUCDLLAq0noQJmmxV"
+// }
+
+ 
