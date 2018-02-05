23998c8e69134b2b218fb9b37a331678c76ea30b
WslyStvnChng <mr.cheng.st@gmail.com^C>
2018-02-05 14:01:24 -0700

    tweets are confimred, need to work on spotify, etc

--- assets/javascript/keys.js
+++ assets/javascript/keys.js
@@ -6,11 +6,6 @@ exports.twitter = {
 };
 
 console.log("This works");
-// exports.twitter = {
-//   consumer_key: "process.env.K5C5tSZyi2UTMIgSrxHbxOYeh",
-//   consumer_secret: "process.env.CO2Fb75lbjuutJrbyNHE6gYIxgY8K5bcsAnRkMjM2HmSD24QC5",
-//   access_token_key: "process.env.958520639815495681-NA5SwL3n9KM6Oz16ckPNHl4KfBxpF2g",
-//   access_token_secret: "process.env.L7BuXaHMo3pZH9TXeomRJ9IlwjGUvUCDLLAq0noQJmmxV"
-// }
 
  
+
--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -5,21 +5,23 @@
 // var spotifyKey = require("node-spotify-api");
 var apiKeys = require("./keys.js");
 var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 
 // ---Function ---
 
 // Can use function declaration or expression
 var myTweets = function(newTweet) {
   var client = new Twitter(apiKeys.twitter);
-    // console.log("tweets are fun");
-  var params = { screen_name: "chengman303" };
+    console.log("tweets are fun");
+  var params = { screen_name: "chengman303", count: 20 };
   
   if(newTweet) {
     // do this if user provides new tweet
-    console.log("Tweet the Fung Bros " + newTweet);
+    console.log("Tweet this " + newTweet);
   } else {
 
     // else print out 20 latest tweets
+    
       client.get("statuses/user_timeline", params, function(
       error,
       tweets,
@@ -32,23 +34,53 @@ var myTweets = function(newTweet) {
         // 
         for (var i = 0; i < tweetCount; i++) {
             console.log(tweets[i].text);
-            // console.log("+++++++++++++++++");
-            // console.log(" ");
+            console.log("+++++++++++++++++");
+            console.log(" ");
         }
       }
     });
   }
 }
-var mySong = function() {
-  // console.log("my songs is the best");
+var mySong = function(songRequest) {
+  var Spotify = new Spotify ({
+    id: "efaa22009ac04c47bc4de7a6d272c8e2",
+    secret: "456cc28a892642ad853a5f907031aaa1"
+  });
+  var songName = "";
+  if(process.argv.length > 3) {
+    for (var i = 3; i < process.argv.length; i++) {
+      if( i > 3 && i < process.argv.length) {
+        songName = songName + '+' + (process.argv[i])
+      } else {
+        songName += process.argv[i];
+      }
+    }
 
-
-
-
-
-
+    spotify
+      .request("https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx")
+      .then(function(data) {
+        console.log(data);
+        console.log("==================================================");
+        console.log('Artist: ' + data.album.artists[0].name);
+        console.log('Song name: ' + data.name);
+        console.log("Preview link: " + data.preview_url);
+        console.log("Album: " + data.album.name);
+        console.log("==================================================")
+      })
+      .catch(function(err) {
+        console.error("Error occurred: " + err);
+      });
+  }
+  
 
 }
+
+
+
+
+
+
+
 
 var myMovie = function() {
   console.log("my movie is the best");
